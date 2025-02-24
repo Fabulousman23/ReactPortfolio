3eb012e5dd4d7a2b501dbd1b0da1b8d7cbfc232f
Fabulousman23 <andrew.makiev@gmail.com>
2025-02-23 18:13:14 -0800

    !

--- next.config.js
+++ next.config.js
@@ -14,3 +14,27 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+```
+
+# example netlify.toml
+[build]
+  command = "next build"
+  functions = "netlify/functions"
+  publish = ".next"
+
+[context.production.environment]
+  NETLIFY_NEXT_PLUGIN_SKIP = "true"
+
+[[redirects]]
+  from = "/*"
+  to = "/index.html"
+  status = 200
+
+## (optional) Settings for Netlify Dev
+## https://github.com/netlify/cli/blob/main/docs/netlify-dev.md#project-detection
+#[dev]
+#  command = "yarn start" # Command to start your dev server
+#  port = 3000 # Port that the dev server will be listening on
+#  publish = "dist" # Folder with the static content for _redirect file
+
+## more info on configuring this file: https://docs.netlify.com/configure-builds/file-based-configuration/
