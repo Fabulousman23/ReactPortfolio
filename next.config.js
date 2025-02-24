10c3c1e24a53b7c45cd85425b8329bffaee4e5a4
Fabulousman23 <andrew.makiev@gmail.com>
2025-02-23 18:34:11 -0800

    fix

--- next.config.js
+++ next.config.js
@@ -4,28 +4,6 @@ const nextConfig = {
   // other configurations
 };
 
+const example = `This is an improperly closed template literal`;
+
 module.exports = nextConfig;
-```
-
-# example netlify.toml
-[build]
-  command = "next build"
-  functions = "netlify/functions"
-  publish = ".next"
-
-[context.production.environment]
-  NETLIFY_NEXT_PLUGIN_SKIP = "true"
-
-[[redirects]]
-  from = "/*"
-  to = "/index.html"
-  status = 200
-
-## (optional) Settings for Netlify Dev
-## https://github.com/netlify/cli/blob/main/docs/netlify-dev.md#project-detection
-#[dev]
-#  command = "yarn start" # Command to start your dev server
-#  port = 3000 # Port that the dev server will be listening on
-#  publish = "dist" # Folder with the static content for _redirect file
-
-## more info on configuring this file: https://docs.netlify.com/configure-builds/file-based-configuration/
