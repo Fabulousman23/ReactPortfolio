a16ce9590b56f61364f6c6847c55a6920e4dc020
Fabulousman23 <andrew.makiev@gmail.com>
2025-02-23 18:21:50 -0800

    fixing the build

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
-// filepath: /Users/andrewmakiev/Documents/ReactPortfolio/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: ".next",
-  async rewrites() {
-    return [
-      {
-        source: "/index.html",
-        destination: "/200",
-      },
-    ];
-  },
+  // other configurations
 };
 
 module.exports = nextConfig;
