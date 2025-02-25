235369068c5bb874591cdd36097428759298ac0b
Fabulousman23 <andrew.makiev@gmail.com>
2025-02-24 22:32:42 -0800

    !

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -23,7 +23,7 @@ const Footer = ({ }) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With ❤ {" "}
+        {/* Made With ❤ {" "} */}
         <Link href=" ">
           <a className="underline underline-offset-1"> </a>
         </Link>
