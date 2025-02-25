28e82f23457775b91295758159cfccaa1ae6039c
Fabulousman23 <andrew.makiev@gmail.com>
2025-02-25 13:36:17 -0800

    fixing errors

--- components/MyComponent.js
+++ components/MyComponent.js
@@ -1,36 +1,7 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-const MyComponent = () => (
-  <Link href="/about">
-    About
-  </Link>
-);
+const MyComponent = () => <Link href="/about">About</Link>;
 
 export default MyComponent;
-```
-
-{
-  "name": "your-app-name",
-  "version": "1.0.0",
-  "description": "Your app description",
-  "main": "index.js",
-  "scripts": {
-    "start": "next start",
-    "build": "next build",
-    "dev": "next dev"
-  },
-  "engines": {
-    "node": "22.x",
-    "npm": ">=6.0.0"
-  },
-  "dependencies": {
-    "next": "^12.0.0",
-    "react": "^18.2.0",
-    "react-dom": "^18.2.0",
-    "express": "^4.17.1",
-    "cors": "^2.8.5"
-  },
-  "devDependencies": {
-    "nodemon": "^2.0.7"
-    }
+``;
