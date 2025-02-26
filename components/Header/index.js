7dad186b83e22722b3055ef5492d4814c058fc7c
Fabulousman23 <andrew.makiev@gmail.com>
2025-02-26 12:06:18 -0800

    fixing errors

--- components/Header/index.js
+++ components/Header/index.js
@@ -39,8 +39,9 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   >
                     <img
                       className="h-6"
-                      src={`/images/${theme === "dark" ? "moon.svg" : "sun.svg"
-                        }`}
+                      src={`/images/${
+                        theme === "dark" ? "moon.svg" : "sun.svg"
+                      }`}
                     ></img>
                   </Button>
                 )}
@@ -48,21 +49,23 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                 <Popover.Button>
                   <img
                     className="h-5"
-                    src={`/images/${!open
-                      ? theme === "dark"
-                        ? "menu-white.svg"
-                        : "menu.svg"
-                      : theme === "light"
+                    src={`/images/${
+                      !open
+                        ? theme === "dark"
+                          ? "menu-white.svg"
+                          : "menu.svg"
+                        : theme === "light"
                         ? "cancel.svg"
                         : "cancel-white.svg"
-                      }`}
+                    }`}
                   ></img>
                 </Popover.Button>
               </div>
             </div>
             <Popover.Panel
-              className={`absolute right-0 z-10 w-11/12 p-4 ${theme === "dark" ? "bg-slate-800" : "bg-white"
-                } shadow-md rounded-md`}
+              className={`absolute right-0 z-10 w-11/12 p-4 ${
+                theme === "dark" ? "bg-slate-800" : "bg-white"
+              } shadow-md rounded-md`}
             >
               {!isBlog ? (
                 <div className="grid grid-cols-1">
@@ -82,7 +85,9 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:andrew.makiev@gmail.com")}
+                    onClick={() =>
+                      window.open("mailto:andrew.makiev@gmail.com")
+                    }
                   >
                     Contact
                   </Button>
@@ -105,7 +110,9 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:andrew.makiev@gmail.com")}
+                    onClick={() =>
+                      window.open("mailto:andrew.makiev@gmail.com")
+                    }
                   >
                     Contact
                   </Button>
@@ -116,8 +123,9 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
         )}
       </Popover>
       <div
-        className={`mt-10 hidden flex-row items-center justify-between sticky ${theme === "light" && "bg-white"
-          } dark:text-white top-0 z-10 tablet:flex`}
+        className={`mt-10 hidden flex-row items-center justify-between sticky ${
+          theme === "light" && "bg-white"
+        } dark:text-white top-0 z-10 tablet:flex`}
       >
         <h1
           onClick={() => router.push("/")}
@@ -141,7 +149,9 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:andrew.makiev@gmail.com")}>
+            <Button
+              onClick={() => window.open("mailto:andrew.makiev@gmail.com")}
+            >
               Contact
             </Button>
             {mounted && theme && data.darkMode && (
@@ -170,7 +180,9 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:andrew.makiev@gmail.com")}>
+            <Button
+              onClick={() => window.open("mailto:andrew.makiev@gmail.com")}
+            >
               Contact
             </Button>
 
