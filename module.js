59be679c705baa8b0435347003b3af1fdfac4fe4
Fabulousman23 <andrew.makiev@gmail.com>
2025-02-26 01:15:57 -0800

    fixing bug

--- module.js
+++ module.js
@@ -1,6 +1,10 @@
 module.exports = {
-  plugins: {
-    "@tailwindcss/postcss7-compat": {},
-    autoprefixer: {},
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 };
