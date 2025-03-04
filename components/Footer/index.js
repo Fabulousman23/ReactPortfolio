d815c1e6e1edffaa36a6709c334b3303ae767717
Fabulousman23 <andrew.makiev@gmail.com>
2025-03-03 16:56:00 -0800

    update resume

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,17 +3,17 @@ import Socials from "../Socials";
 import Link from "next/link";
 import Button from "../Button";
 
-const Footer = ({ }) => {
+const Footer = () => {
   return (
     <>
       <div className="mt-5 mx-10 laptop:mt-40 p-2 laptop:p-0">
         <div>
           <h1 className="text-2xl text-bold">Contact.</h1>
           <div className="mt-10">
-            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptop:text-8xl text-bold">
               LET&apos;S WORK
             </h1>
-            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptop:text-8xl text-bold">
               TOGETHER
             </h1>
             <div className="mt-10">
@@ -24,8 +24,8 @@ const Footer = ({ }) => {
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         {/* Made With ❤ {" "} */}
-        <Link href=" ">
-          <a className="underline underline-offset-1"> </a>
+        <Link href="/">
+          <a className="underline underline-offset-1">Home</a>
         </Link>
       </h1>
     </>
