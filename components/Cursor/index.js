9e688558efc961d62580839581cbf34400dd5ec4
Fabulousman23 <andrew.makiev@gmail.com>
2025-03-26 23:18:53 -0700

    update cursor

--- components/Cursor/index.js
+++ components/Cursor/index.js
@@ -1,42 +1,7 @@
-import React, { useEffect, useState } from "react";
-import CustomCursor from "custom-cursor-react";
-import "custom-cursor-react/dist/index.css";
-import { useTheme } from "next-themes";
+import React from "react";
 
 const Cursor = () => {
-  const theme = useTheme();
-  const [mount, setMount] = useState();
-
-  const getCusomColor = () => {
-    if (theme.theme === "dark") {
-      return "#fff";
-    } else if (theme.theme === "light") {
-      return "#000";
-    }
-  };
-
-  useEffect(() => {
-    setMount(true);
-  }, []);
-  return (
-    <>
-      {mount && (
-        <CustomCursor
-          targets={[".link"]}
-          customClass="custom-cursor"
-          dimensions={30}
-          fill={getCusomColor()}
-          smoothness={{
-            movement: 0.2,
-            scale: 0.1,
-            opacity: 0.2,
-          }}
-          targetOpacity={0.5}
-          targetScale={2}
-        />
-      )}
-    </>
-  );
+  return null; // Replace with your own custom cursor logic or leave it empty
 };
 
 export default Cursor;
