c953f7d3f644879ea646dba4625ec87477b8ba6f
Fabulousman23 <andrew.makiev@gmail.com>
2025-03-26 22:09:13 -0700

    fix resume.js

--- pages/resume.js
+++ pages/resume.js
@@ -7,9 +7,9 @@ import Socials from "../components/Socials";
 import Button from "../components/Button";
 import { useTheme } from "next-themes";
 // Data
-import { name, showResume } from "../data/portfolio.json";
-import { resume } from "../data/portfolio.json";
-import data from "../data/portfolio.json";
+import portfolio from "../data/portfolio.json";
+
+const { name, showResume, resume } = portfolio;
 
 const Resume = () => {
   const router = useRouter();
@@ -22,6 +22,7 @@ const Resume = () => {
       router.push("/");
     }
   }, []);
+
   return (
     <>
       {process.env.NODE_ENV === "development" && (
@@ -31,10 +32,10 @@ const Resume = () => {
           </Button>
         </div>
       )}
-      {data.showCursor && <Cursor />}
+      {portfolio.showCursor && <Cursor />}
       <div
         className={`container mx-auto mb-10 ${
-          data.showCursor && "cursor-none"
+          portfolio.showCursor && "cursor-none"
         }`}
       >
         <Header isBlog />
