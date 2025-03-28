437cd81547ab612d17b62dc8c090e192a5ce7b0d
Fabulousman23 <andrew.makiev@gmail.com>
2025-03-27 22:41:07 -0700

    ok

--- utils/index.js
+++ utils/index.js
@@ -32,8 +32,8 @@ const HeroImage = () => {
     <Image
       src="https://images.unsplash.com/photo-1542204165-65bf26472b9b?q=80&w=3948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       alt="Descriptive Alt Text"
-      width={3948}
-      height={2560}
+      width={948}
+      height={560}
       priority
       layout="responsive" // Ensures the image scales properly
     />
